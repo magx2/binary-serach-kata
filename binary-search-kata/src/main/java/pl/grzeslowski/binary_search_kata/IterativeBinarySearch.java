c7d738de101f68f24bbfe539af2fe1e8060b45a2
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-05 18:44:31 +0100

    Added 3 mocks

---
+++ binary-search-kata/src/main/java/pl/grzeslowski/binary_search_kata/IterativeBinarySearch.java
@@ -0,0 +1,25 @@
+package pl.grzeslowski.binary_search_kata;
+
+public class IterativeBinarySearch<T> implements BinarySearchArray<T> {
+
+    public void add(final T element) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean remove(final T element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean contains(final T element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public int findIndex(final T element) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
